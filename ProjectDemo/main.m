2dbfa53c5a5344215a324a422aefb8b8ffab1134
liu464698104 <meijust@sina.cn>
2014-08-27 14:09:29 +0800

    Initial Commit

---
+++ ProjectDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ProjectDemo
+//
+//  Created by 时代合盛 on 14-8-27.
+//  Copyright (c) 2014年 时代和盛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CBAppDelegate class]));
+    }
+}
---
+++ ProjectDemoTests/ProjectDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  ProjectDemoTests.m
+//  ProjectDemoTests
+//
+//  Created by 时代合盛 on 14-8-27.
+//  Copyright (c) 2014年 时代和盛. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ProjectDemoTests : XCTestCase
+
+@end
+
+@implementation ProjectDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
